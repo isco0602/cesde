781635f31b6375b0e7443ac5e532562d5c3c06e1
francisco valencia <valenciafjvm16@hotmail.com>
2018-09-19 15:50:37 -0500

    phptodo

---
+++ project5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="registro.php"method="get">
+         <label>nombre</label>
+         <input type="text" name="nombre" placeholder="nombre">
+         <input type="submit" value="enviar">
+        </form>       
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
